14f212be2516b639cdd509f714a51f8365e2f49c
vi1281 <vika30072004@gmail.com>
2024-12-20 03:01:41 +0100

    cc

---
+++ register.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDD7wYR16lWYvHN6FlvZu9UpLzXW8dw5Mo",
+    authDomain: "login-72844.firebaseapp.com",
+    projectId: "login-72844",
+    storageBucket: "login-72844.firebasestorage.app",
+    messagingSenderId: "856995387716",
+    appId: "1:856995387716:web:00a49160d4a84e08c29f11"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
